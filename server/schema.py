439334dfde9cfd8ac99dc7ba97a1368f4aaf16b5
samparent97 <samuelparent97@gmail.com>
2023-12-06 13:17:35 -0500

    Working schema

--- server/schema.py
+++ server/schema.py
@@ -1,7 +1,7 @@
 import yaml
 from jsonschema import validate
 
-def validate_tags(tags_file_path, schema_file_path):
+def validate_tags(tags_file_path, schema_file_path) -> bool:
     # Load YAML schema
     with open(schema_file_path, 'r') as schema_file:
         schema = yaml.safe_load(schema_file)
@@ -13,13 +13,24 @@ def validate_tags(tags_file_path, schema_file_path):
     # Validate the data against the schema
     try:
         validate(tags_data, schema)
-        print("YAML document is valid against the schema.")
+        return True
     except Exception as e:
         print(f"Validation error: {e}")
+        return False
 
 # Paths to your YAML files
-tags_yaml_path = "tags.yaml"
-tags_schema_yaml_path = "tags_schema.yaml"
+tags_schema_yaml_path = "tags_schema.json"
+tags_yaml_path_good = "good_tags.yaml"
+tags_yaml_path_bad = "bad_tags.yaml"
 
 # Validate the YAML data against the schema
-validate_tags(tags_yaml_path, tags_schema_yaml_path)
+if validate_tags(tags_yaml_path_good, tags_schema_yaml_path):
+    print("Passed tes for good yaml")
+else:
+    print("Failed test for good yaml")
+if not validate_tags(tags_yaml_path_bad, tags_schema_yaml_path):
+    print("Passed test for bad yaml")
+else:
+    print("Failed test for bad yaml")
+
+
